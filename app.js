20377af882f5dfe396bac1e24c2656ecb5a9467f
vickygit2227 <78005771+vickygit2227@users.noreply.github.com>
2021-02-03 19:29:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
 //This will be used to run the Script , required to deploy the website
 app.post('/DeployTerraform', function (req, res) {
     var commandToRun = "terraform apply -auto-approve ";
-    console.log("finalCommand->"+ finalCommand);
+    console.log("finalCommand->"+ commandToRun);
     cmd.run(commandToRun);
     res.sendStatus(200);
 });
